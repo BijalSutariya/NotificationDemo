269e0d7a8ef80743446de6ddc9f434ea510be823
Bijal <bijal.matrixhive@gmail.com>
2018-08-11 15:44:51 +0530

    third commit

--- app/src/main/java/e/matrixhive/notificationdemo/MainActivity.java
+++ app/src/main/java/e/matrixhive/notificationdemo/MainActivity.java
@@ -2,12 +2,9 @@ package e.matrixhive.notificationdemo;
 
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -42,5 +39,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
-    
 }
