086dccea1d742acd2de84622c1b85d268df7c0b7
Bijal <bijal.matrixhive@gmail.com>
2018-08-11 15:47:36 +0530

    commit

--- app/src/main/java/e/matrixhive/notificationdemo/MainActivity.java
+++ app/src/main/java/e/matrixhive/notificationdemo/MainActivity.java
@@ -1,13 +1,6 @@
 package e.matrixhive.notificationdemo;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
