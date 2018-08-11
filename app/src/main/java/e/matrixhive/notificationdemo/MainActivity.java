504e7e2b2c0b72ba1732974f68cfd4d4542963ba
Bijal <bijal.matrixhive@gmail.com>
2018-08-11 15:01:00 +0530

    second commit

--- app/src/main/java/e/matrixhive/notificationdemo/MainActivity.java
+++ app/src/main/java/e/matrixhive/notificationdemo/MainActivity.java
@@ -1,13 +1,46 @@
 package e.matrixhive.notificationdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.btn1);
+
+        btnOne.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                viewNotification();
+                break;
+        }
+    }
+
+    private void viewNotification() {
+
+
+    }
+    
 }
