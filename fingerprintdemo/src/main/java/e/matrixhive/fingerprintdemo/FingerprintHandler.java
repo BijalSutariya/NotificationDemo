3b833c13b299ff86b7b938bbdb983aa40f6ad9e6
Bijal <bijal.matrixhive@gmail.com>
2018-08-13 17:41:37 +0530

    finger print commit

---
+++ fingerprintdemo/src/main/java/e/matrixhive/fingerprintdemo/FingerprintHandler.java
@@ -0,0 +1,60 @@
+package e.matrixhive.fingerprintdemo;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.TextView;
+
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+    private Context context;
+
+
+    // Constructor
+    public FingerprintHandler(Context mContext) {
+        context = mContext;
+    }
+
+
+    public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+
+    @Override
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+        this.update("Fingerprint Authentication error\n" + errString, false);
+    }
+
+
+    @Override
+    public void onAuthenticationFailed() {
+        this.update("Fingerprint Authentication failed.", false);
+    }
+
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        ((Activity) context).finish();
+        Intent intent = new Intent(context,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+    }
+
+
+    public void update(String e, Boolean success) {
+        TextView textView = ((Activity) context).findViewById(R.id.title);
+        textView.setText(e);
+    }
+}
