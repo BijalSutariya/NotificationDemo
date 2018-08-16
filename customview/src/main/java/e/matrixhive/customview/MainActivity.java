440329ea45721c44d460505f5cc4b9b628503e44
Bijal <bijal.matrixhive@gmail.com>
2018-08-16 14:48:23 +0530

    customView

---
+++ customview/src/main/java/e/matrixhive/customview/MainActivity.java
@@ -0,0 +1,57 @@
+package e.matrixhive.customview;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
+
+import e.matrixhive.customview.views.CustomView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomView view;
+    private TextView tvBattery;
+    private Handler handler;
+    private Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.view);
+        tvBattery = findViewById(R.id.tv_battery);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int level = (int) batteryLevel();
+
+                tvBattery.setText(MessageFormat.format("BATTERY:{0} %", level));
+
+                view.showImage(level);
+
+                handler.postDelayed(runnable, 2000);
+            }
+        };
+        handler = new Handler();
+        handler.postDelayed(runnable, 0);
+    }
+
+    public float batteryLevel() {
+        Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        if (level == -1 || scale == -1) {
+            return 50.0f;
+        }
+        return ((float) level / (float) scale) * 100.0f;
+    }
+}
---
+++ customview/src/main/java/e/matrixhive/customview/views/CustomView.java
@@ -0,0 +1,115 @@
+package e.matrixhive.customview.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View {
+    private int id;
+    private Paint paintBorder, paintFill;
+    private Path pathBorder, pathFill, pathShape;
+    float segmentLength;
+    float deviation;
+    private Rect rect, rectFill;
+
+
+    public CustomView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+
+    }
+
+    private void init(@Nullable AttributeSet attrs) {
+        paintFill = new Paint();
+        paintFill.setAntiAlias(true);
+
+        paintBorder = new Paint();
+
+        pathBorder = new Path();
+        pathFill = new Path();
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.GRAY);
+        paintFill.setStyle(Paint.Style.FILL);
+        if (id > 90) {
+            pathFill.addRect(0, 0, getWidth(), getHeight(), Path.Direction.CW);
+            paintFill.setColor(Color.GREEN);
+        }
+        if (id > 75 && id <= 90){
+            pathFill.addRect(0, 0, getWidth(), (float) (getHeight()/1.1), Path.Direction.CW);
+            paintFill.setColor(Color.GREEN);
+        }
+        if (id > 60 && id <= 75){
+            pathFill.addRect(0, 0, getWidth(), (float) (getHeight()/1.5), Path.Direction.CW);
+            paintFill.setColor(Color.GREEN);
+        }
+        if (id > 45 && id <= 60){
+            pathFill.addRect(0, 0, getWidth(), getHeight()/2, Path.Direction.CW);
+            paintFill.setColor(Color.GREEN);
+        }
+        if (id > 30 && id <= 45){
+            pathFill.addRect(0, 0, getWidth(), (float) (getHeight()/2.5), Path.Direction.CW);
+            paintFill.setColor(Color.GREEN);
+        }
+        if (id > 20 && id <= 30){
+            pathFill.addRect(0, 0, getWidth(), getHeight()/5, Path.Direction.CW);
+            paintFill.setColor(Color.GREEN);
+        }
+        if (id <= 20){
+            pathFill.addRect(0, 0, getWidth(), getHeight()/10, Path.Direction.CW);
+            paintFill.setColor(Color.RED);
+        }
+        canvas.save();
+        canvas.rotate(
+                180, // degrees
+                canvas.getWidth() / 2,
+                canvas.getHeight() / 2
+        );
+        canvas.drawPath(pathFill, paintFill);
+        canvas.restore();
+
+
+
+        paintFill.setColor(Color.BLACK);
+        paintFill.setTextSize(50);
+
+        paintFill.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(String.valueOf(id)+"%", getWidth()/2, getHeight()/2, paintFill);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    public void showImage(int level) {
+        id = level;
+    }
+}
+
+
